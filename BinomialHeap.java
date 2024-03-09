7c090af81106216daff34165d6c9b50f200a4d03
Ishay <ishay.yemini@gmail.com>
2024-03-09 17:54:41 +0200

    Some refactoring

--- BinomialHeap.java
+++ BinomialHeap.java
@@ -10,135 +10,125 @@
  * An implementation of binomial heap over non-negative integers.
  * Based on exercise from previous semester.
  */
-public class BinomialHeap
-{
+public class BinomialHeap {
 	public int size;
 	public HeapNode last;
 	public HeapNode min;
 
+	public BinomialHeap() {}
+
+	public BinomialHeap(int size, HeapNode last, HeapNode min) {
+		this.size = size;
+		this.last = last;
+		this.min = min;
+	}
+
 	/**
-	 * 
 	 * pre: key > 0
-	 *
 	 * Insert (key,info) into the heap and return the newly generated HeapItem.
-	 *
 	 */
-	public HeapItem insert(int key, String info) 
-	{    
-		return; // should be replaced by student code
+	public HeapItem insert(int key, String info) {
+		HeapNode newNode = new HeapNode(key, info);
+
+		this.meld(newNode.toHeap());
+
+		return newNode.item;
 	}
 
 	/**
-	 * 
 	 * Delete the minimal item
-	 *
 	 */
-	public void deleteMin()
-	{
-		if size == 0 {
-			return null;
-	    }
-
-		return; // should be replaced by student code
-
+	public void deleteMin() {
+		if (size == 0) return;
+		// find new min
+		// last is the same
 	}
 
 	/**
-	 * 
 	 * Return the minimal HeapItem
-	 *
 	 */
-	public HeapItem findMin()
-	{
+	public HeapItem findMin() {
 		return min.item;
-	} 
+	}
 
 	/**
-	 * 
 	 * pre: 0 < diff < item.key
-	 * 
-	 * Decrease the key of item by diff and fix the heap. 
-	 * 
+	 * Decrease the key of item by diff and fix the heap.
 	 */
-	public void decreaseKey(HeapItem item, int diff) 
-	{    
-		return; // should be replaced by student code
+	public void decreaseKey(HeapItem item, int diff) {
+		// should be replaced by student code
 	}
 
 	/**
-	 * 
 	 * Delete the item from the heap.
-	 *
 	 */
-	public void delete(HeapItem item) 
-	{    
-		return; // should be replaced by student code
+	public void delete(HeapItem item) {
+		// should be replaced by student code
 	}
 
 	/**
-	 * 
 	 * Meld the heap with heap2
-	 *
 	 */
-	public void meld(BinomialHeap heap2)
-	{
-		return; // should be replaced by student code   		
+	public void meld(BinomialHeap heap2) {
+		// should be replaced by student code
 	}
 
 	/**
-	 * 
 	 * Return the number of elements in the heap
-	 *   
 	 */
-	public int size()
-	{
+	public int size() {
 		return size; // should be replaced by student code
 	}
 
 	/**
-	 * 
 	 * The method returns true if and only if the heap
 	 * is empty.
-	 *   
 	 */
-	public boolean empty()
-	{
-		if size == 0 {
-			return true;
-	}
-		return false; // should be replaced by student code
+	public boolean empty() {
+		return size == 0;
 	}
 
 	/**
-	 * 
 	 * Return the number of trees in the heap.
-	 * 
 	 */
-	public int numTrees()
-	{
+	public int numTrees() {
 		return 0; // should be replaced by student code
 	}
 
 	/**
 	 * Class implementing a node in a Binomial Heap.
-	 *  
 	 */
-	public class HeapNode{
+	public class HeapNode {
 		public HeapItem item;
 		public HeapNode child;
 		public HeapNode next;
 		public HeapNode parent;
 		public int rank;
+
+		public HeapNode(int key, String info) {
+			this.item = new HeapItem(this, key, info);
+		}
+
+		/**
+		 * Return the node as a heap
+		 */
+		public BinomialHeap toHeap() {
+			return new BinomialHeap(1, this, this);
+		}
 	}
 
 	/**
 	 * Class implementing an item in a Binomial Heap.
-	 *  
 	 */
-	public class HeapItem{
+	public class HeapItem {
 		public HeapNode node;
 		public int key;
 		public String info;
-	}
 
+		public HeapItem(HeapNode node, int key, String info) {
+			this.node = node;
+			this.key = key;
+			this.info = info;
+		}
+	}
 }
